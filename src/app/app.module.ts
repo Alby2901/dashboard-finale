e829de8208c9f02892d0ecfe1fd73c53e6a0a8c3
Alby2901 <a.azzola@scriptanet.it>
2022-10-05 21:54:48 +0200

    20221005-02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './componenti/login/login.component';
+import { RegisterComponent } from './componenti/register/register.component';
+import { DashboardComponent } from './componenti/dashboard/dashboard.component';
+import { Pagina1Component } from './componenti/pagina1/pagina1.component';
+import { Pagina2Component } from './componenti/pagina2/pagina2.component';
+import { Pagina3Component } from './componenti/pagina3/pagina3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    Pagina1Component,
+    Pagina2Component,
+    Pagina3Component
   ],
   imports: [
     BrowserModule,
