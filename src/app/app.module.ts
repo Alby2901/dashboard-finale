c4fd601bbfdd93281370ea466a74001ad5b5d483
Alby2901 <a.azzola@scriptanet.it>
2022-10-06 22:35:55 +0200

    20221006-03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './componenti/login/login.component';
@@ -43,7 +44,8 @@ import { Pagina3Component } from './componenti/pagina3/pagina3.component';
     MatSidenavModule,
     MatListModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
